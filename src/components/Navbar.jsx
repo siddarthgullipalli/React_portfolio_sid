e4ca0eed5f62ccd4459243444d5ec386e34dd1ba
Siddarth Gullipalli <sgullipalli1@gmail.com>
2024-06-21 08:59:34 +0530

    2nd commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,30 @@ import {FaGithub} from "react-icons/fa"
 import {FaSquareXTwitter} from "react-icons/fa6"
 import {FaInstagram} from "react-icons/fa"
 import React from 'react'
+import { motion } from 'framer-motion';
+
+const con = (delay) => {
+  return ({
+    hidden:{x:-100 , opacity :0.5},
+    visible1: {
+      x:0 ,
+      opacity :1,
+      transition : {duration :0.8, delay : delay}
+    },
+    visible: {
+        x:0 ,
+        opacity :1,
+        transition : {duration :0.5, delay : delay}
+    }
+  }
+   
+  )
+}
 
 const Navbar = () => {
   return <nav className='mb-20 flex items-center justify-between py-6'>
     <div className='flex flex-shrink-0 items-center'>
-    <span className='bg-gradient-to-r from-pink-300 visa-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent'>.. /Sid.Dev</span>
+    <motion.span variants={con(0)} initial='hidden' animate = 'visible'  className='bg-gradient-to-r from-pink-300 visa-slate-500 to-purple-500 bg-clip-text text-2xl tracking-tight text-transparent'>.. /Sid<motion.span variants={con(0)} initial='hidden' animate = 'visible1'>.Dev</motion.span></motion.span>
       {/* <img className="mx-2 w-10" src={logo} alt=''/> */}
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
@@ -21,3 +40,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,19 +4,32 @@ import { DiDocker } from "react-icons/di";
 import { DiSqllite } from "react-icons/di"
 import {BiLogoPostgresql} from "react-icons/bi"
 import {FaNodeJs} from "react-icons/fa"
+import { motion } from 'framer-motion';
+
+const iconVariants = (duration) => ({
+  initial :{ y: -10},
+  animate :{
+    y: [10, -10],
+    transition :{duration ,
+    ease:'linear',
+    repeat : Infinity,
+    repeatType: 'reverse',
+  },
+},
+})
 
 
 const Technologies = () => {
   return  <div className="border-b border-neutral-800 pb-24">
-    <h2 className="my-20 text-center text-4xl">Technologies</h2>
-    <div className="flex flex-wrap items-center justify-center gap-4">
-<div className="rounded-2xl border-4 border-neutral-800 p-4"><RiReactjsLine className="text-7xl text-cyan-400"/></div>
-<div className="rounded-2xl border-4 border-neutral-800 p-4"><DiDjango className="text-7xl text-green-600"/></div>
-<div className="rounded-2xl border-4 border-neutral-800 p-4"><DiDocker className="text-7xl text-cyan-400"/></div>
-<div className="rounded-2xl border-4 border-neutral-800 p-4"><DiSqllite  className="text-7xl text-sky-400"/></div>
-<div className="rounded-2xl border-4 border-neutral-800 p-4"><BiLogoPostgresql className="text-7xl text-sky-700"/></div>
-<div className="rounded-2xl border-4 border-neutral-800 p-4"><FaNodeJs className="text-7xl text-green-500"/></div>
-</div>
+    <motion.h2 whileInView={{opacity:1 ,y:0 ,x:0 }} initial = {{opacity:0 ,y:-100 ,x:-100 }} transition={{duration:1.5}} className="my-20 text-center text-4xl">Technologies</motion.h2>
+    <motion.div whileInView={{opacity:1 ,x:0}} initial = {{opacity:0 ,x:-100 }} transition={{duration:1.5}} className="flex flex-wrap items-center justify-center gap-4">
+<motion.div  variants={iconVariants(2.5)} initial='initial' animate='animate' className="rounded-2xl border-4 border-neutral-800 p-4"><RiReactjsLine className="text-7xl text-cyan-400"/></motion.div>
+<motion.div variants={iconVariants(2)} initial='initial' animate='animate' className="rounded-2xl border-4 border-neutral-800 p-4"><DiDjango className="text-7xl text-green-600"/></motion.div>
+<motion.div variants={iconVariants(6)} initial='initial' animate='animate' className="rounded-2xl border-4 border-neutral-800 p-4"><DiDocker className="text-7xl text-cyan-400"/></motion.div>
+<motion.div variants={iconVariants(4.5)} initial='initial' animate='animate' className="rounded-2xl border-4 border-neutral-800 p-4"><DiSqllite  className="text-7xl text-sky-400"/></motion.div>
+<motion.div variants={iconVariants(3)} initial='initial' animate='animate' className="rounded-2xl border-4 border-neutral-800 p-4"><BiLogoPostgresql className="text-7xl text-sky-700"/></motion.div>
+<motion.div variants={iconVariants(5)} initial='initial' animate='animate' className="rounded-2xl border-4 border-neutral-800 p-4"><FaNodeJs className="text-7xl text-green-500"/></motion.div>
+</motion.div>
 
   </div>
   
